da84c1686a4ac213a11de58290a645236e6d43a7
interzone <interzone@gmail.com>
2018-12-27 13:56:30 +0200

    create project

---
+++ src/main/java/ua/squirrel/SquirrelApplication.java
@@ -0,0 +1,14 @@
+package ua.squirrel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquirrelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquirrelApplication.class, args);
+	}
+
+}
+
